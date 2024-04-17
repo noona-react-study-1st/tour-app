6bb0d3da223b4b2a959f3f39564fbb5c3f871898
whynot-byoul <whynot@hubilon.com>
2024-04-17 10:06:37 +0900

    1. [add] 공통정보조회 useQuery 추가 2. git branch add / first commit test

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,3 +1,32 @@
+import {useParams} from 'react-router-dom';
+import {useFetchDetailCommonQuery} from '../hooks/useFetchDetailCommon';
+import {Alert, Spinner} from 'react-bootstrap';
+
 export default function DetailPage() {
+  const {contentId} = useParams();
+  console.log(contentId);
+  const {data, isLoading, isError, error} = useFetchDetailCommonQuery(contentId);
+  console.log(data, isLoading);
+  if (data) {
+    console.log(data);
+  }
+  if (isLoading) {
+    return (
+      <div className='loading-box'>
+        <Spinner animation='border' role='status'>
+          <span className='visually-hidden'>Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className='loading-box'>
+        <Alert variant='dark' bg='dark' data-bs-theme='dark'>
+          {error.message}
+        </Alert>
+      </div>
+    );
+  }
   return <>Detail page</>;
 }
