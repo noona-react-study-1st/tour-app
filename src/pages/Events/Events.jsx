0fbc9eb9e4a945ea265d991a97780376491aa8e3
KIMMIJU <hviezda1212@gmail.com>
2024-04-19 02:13:11 +0900

    [refactor] renderpagination 분리

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col, Container, Pagination, Spinner, Alert } from 'react-bootstrap';
+import { Row, Col, Container, Spinner, Alert } from 'react-bootstrap';
 import { useFetchEventQuery } from '../../hooks/useFetchEvent';
 import { useFetchEventCarouselQuery } from '../../hooks/useFetchEventCarousel';
 import EventCarousel from '../Events/components/EventCarousel/EventCarousel';
 import EventCard from '../Events/components/EventCard/EventCard';
 import EventList from '../Events/components/EventList/EventList';
+import renderPagination from '../Events/components/EventPagination/renderPagination'; 
 import './Events.style.css';
 
 const EventsPage = () => {
@@ -91,66 +92,6 @@ const EventsPage = () => {
         )
     : [];
 
-  const renderPagination = (currentPage, handlePaginate, totalItems) => {
-    const totalPages = Math.ceil(totalItems / eventsPerPage);
-    const totalPagesToShow = 5; // 한 묶음에 표시할 페이지 수
-    const totalGroups = Math.ceil(totalPages / totalPagesToShow); // 전체 묶음 수
-    const currentGroup = Math.ceil(currentPage / totalPagesToShow); // 현재 페이지가 속한 묶음
-
-    // 현재 묶음의 시작 페이지와 끝 페이지 계산
-    const startPage = (currentGroup - 1) * totalPagesToShow + 1;
-    const endPage = Math.min(startPage + totalPagesToShow - 1, totalPages);
-
-    const paginationItems = [];
-
-    // 처음 페이지, 이전 페이지 버튼 추가
-    paginationItems.push(
-      <Pagination.First
-        key='first'
-        onClick={() => handlePaginate(1)}
-        disabled={currentPage === 1}
-      />,
-      <Pagination.Prev
-        key='prev'
-        onClick={() => handlePaginate(Math.max(currentPage - 1, 1))}
-        disabled={currentPage === 1}
-      />
-    );
-
-    // 페이지 번호 추가
-    for (let i = startPage; i <= endPage; i++) {
-      paginationItems.push(
-        <Pagination.Item
-          key={i}
-          active={i === currentPage}
-          onClick={() => handlePaginate(i)}
-        >
-          {i}
-        </Pagination.Item>
-      );
-    }
-
-    // 다음 페이지, 마지막 페이지 버튼 추가
-    paginationItems.push(
-      <Pagination.Next
-        key='next'
-        onClick={() => handlePaginate(Math.min(currentPage + 1, totalPages))}
-        disabled={currentPage === totalPages}
-      />,
-      <Pagination.Last
-        key='last'
-        onClick={() => handlePaginate(totalPages)}
-        disabled={currentPage === totalPages}
-      />
-    );
-
-    return (
-      <Pagination className='justify-content-center'>
-        {paginationItems}
-      </Pagination>
-    );
-  };
-
   if (isLoading || isImgLoading) {
     return (
       <div className='loading-box'>
@@ -225,7 +166,8 @@ const EventsPage = () => {
           {renderPagination(
             cardCurrentPage,
             handleCardPaginate,
-            displayData.filter((event) => event.cat2 === 'A0207').length
+            displayData.filter((event) => event.cat2 === 'A0207').length,
+            eventsPerPage
           )}
         </div>
       ) : (
@@ -240,7 +182,8 @@ const EventsPage = () => {
           {renderPagination(
             listCurrentPage,
             handleListPaginate,
-            displayData.filter((event) => event.cat2 === 'A0208').length
+            displayData.filter((event) => event.cat2 === 'A0208').length,
+            eventsPerPage
           )}
         </div>
       )}
