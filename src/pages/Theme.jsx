ec30905301609a868d32b1306ccae9f786ecc42f
HONG SE HYUN <sese0733@naver.com>
2024-04-21 15:17:57 +0900

    style: 주석 및 안 쓰는 hook 삭제

--- src/pages/Theme.jsx
+++ src/pages/Theme.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { api } from '../utils/http';
 import { useNavigate } from 'react-router-dom';
