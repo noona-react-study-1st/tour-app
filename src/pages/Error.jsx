3911ae1406ea75694716e6f3be35d731d6127fda
Youngjun Ha <dudwnsk011@gmail.com>
2024-04-21 19:33:53 +0900

    fix/error-page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,29 @@
+import { useRouteError } from 'react-router-dom';
 import '../pages/SearchPage/Common.style.css';
+import MainNavigation from '../components/MainNavigation/MainNavigation';
 
 const ErrorPage = () => {
+  const error = useRouteError();
+
+  let title = 'An error occurred!';
+  let message = 'Something went wrong...';
+
+  if (error.code === 500) {
+    message = error.data.message;
+  }
+
+  if (error.code === 404) {
+    title = 'Not found!';
+    message = 'Could not find resources or pages';
+  }
+
   return (
     <div>
-      <h1>Unexpected Application Error!</h1>
-      <p>We apologize for the inconvenience. Please try again later.</p>
+      <MainNavigation />
+      <main className='d-flex flex-column justify-content-center align-items-center mt-5'>
+        <h1>{title}</h1>
+        <p>{message}</p>
+      </main>
     </div>
   );
 };
