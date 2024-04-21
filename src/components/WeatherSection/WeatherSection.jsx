a5124982f7d5b62949551b220a93a0d995d561f8
whynot-byoul <whynot.byoul@gmail.com>
2024-04-21 11:42:06 +0900

    지역 페이지에 미세먼지 추가

--- src/components/WeatherSection/WeatherSection.jsx
+++ src/components/WeatherSection/WeatherSection.jsx
@@ -1,13 +1,14 @@
-import { useFetchWeatherInfoQuery } from '../../hooks/useFetchWeatherInfo';
-import { useWeatherStore } from '../../store/weather';
+import { useFetchWeatherInfoQuery } from "../../hooks/useFetchWeatherInfo";
+import { useWeatherStore } from "../../store/weather";
 import {
   skyType,
   rainType as precipitationType,
   getWeatherIconClass,
-} from '../../constants/weather';
-import './WeatherSection.style.css';
-import weatherIcon from '../../assets/weather/weather-icon.png';
-import { Spinner } from 'react-bootstrap';
+} from "../../constants/weather";
+import "./WeatherSection.style.css";
+import weatherIcon from "../../assets/weather/weather-icon.png";
+import { Spinner } from "react-bootstrap";
+import MainAirStatus from "../MainAirStatus/MainAirStatus";
 
 export default function WeatherSection() {
   const { weatherArea } = useWeatherStore();
@@ -38,7 +39,7 @@ export default function WeatherSection() {
   }
   if (isLoading) {
     content = (
-      <section className='forecast-skeleton'>
+      <section className="forecast-skeleton">
         <Spinner />
       </section>
     );
@@ -46,7 +47,7 @@ export default function WeatherSection() {
 
   if (isError) {
     content = (
-      <section className='forecast-skeleton'>
+      <section className="forecast-skeleton">
         <span>{error.message}</span>
       </section>
     );
@@ -55,20 +56,20 @@ export default function WeatherSection() {
   if (data) {
     const itemsArray = data.response.body.items.item;
 
-    const tmpData = itemsArray.filter((row) => row.category === 'TMP');
-    const skyData = itemsArray.filter((row) => row.category === 'SKY');
-    const windData = itemsArray.filter((row) => row.category === 'WSD');
-    const humidityData = itemsArray.filter((row) => row.category === 'REH');
-    const rainProb = itemsArray.filter((row) => row.category === 'POP');
-    const rainType = itemsArray.filter((row) => row.category === 'PTY');
-    const maxTemp = itemsArray.filter((row) => row.category === 'TMX');
-    const minTemp = itemsArray.filter((row) => row.category === 'TMN');
+    const tmpData = itemsArray.filter((row) => row.category === "TMP");
+    const skyData = itemsArray.filter((row) => row.category === "SKY");
+    const windData = itemsArray.filter((row) => row.category === "WSD");
+    const humidityData = itemsArray.filter((row) => row.category === "REH");
+    const rainProb = itemsArray.filter((row) => row.category === "POP");
+    const rainType = itemsArray.filter((row) => row.category === "PTY");
+    const maxTemp = itemsArray.filter((row) => row.category === "TMX");
+    const minTemp = itemsArray.filter((row) => row.category === "TMN");
 
     const startIndex = findStartIndex(tmpData);
 
-    let weatherState = '';
+    let weatherState = "";
 
-    if (rainType[startIndex].fcstValue === '0') {
+    if (rainType[startIndex].fcstValue === "0") {
       weatherState = skyType[skyData[startIndex].fcstValue];
     } else {
       weatherState = precipitationType[rainType[startIndex].fcstValue];
@@ -76,54 +77,59 @@ export default function WeatherSection() {
     // console.log(itemsArray);
 
     content = (
-      <section className='forecast-section'>
-        <div className='weather-temp-box'>
-          <div className='temp-box'>
-            <div className='curr-temp'>{tmpData[startIndex].fcstValue}°C</div>
-            <div>
-              {Math.round(maxTemp[0].fcstValue)}°C /{' '}
-              {Math.round(minTemp[0].fcstValue)}°C
+      <>
+        <section className="forecast-section">
+          <div className="weather-temp-box">
+            <div className="temp-box">
+              <div className="curr-temp">{tmpData[startIndex].fcstValue}°C</div>
+              <div>
+                {Math.round(maxTemp[0].fcstValue)}°C /{" "}
+                {Math.round(minTemp[0].fcstValue)}°C
+              </div>
+            </div>
+            <div className="weather-state-section">
+              <div
+                className={`weather-icon ${getWeatherIconClass(weatherState)}`}
+                style={{
+                  backgroundImage: `url("${weatherIcon}")`,
+                }}
+              />
+              <div>{weatherState}</div>
             </div>
           </div>
-          <div className='weather-state-section'>
-            <div
-              className={`weather-icon ${getWeatherIconClass(weatherState)}`}
-              style={{
-                backgroundImage: `url("${weatherIcon}")`,
-              }}
-            />
-            <div>{weatherState}</div>
+          <div className="etc-box">
+            <div>풍속 {windData[startIndex].fcstValue} m/s</div>
+            <div>습도 {humidityData[startIndex].fcstValue}%</div>
           </div>
+          <div className="weather-slider overflow-auto">
+            <ul>
+              <li style={{ width: "5rem", fontWeight: "700" }}>시간</li>
+              {tmpData.slice(startIndex, startIndex + 24).map((row, index) => (
+                <li key={index}>{row.fcstTime.substring(0, 2)}시</li>
+              ))}
+            </ul>
+            <ul>
+              <li style={{ width: "5rem", fontWeight: "700" }}>기온</li>
+              {tmpData.slice(startIndex, startIndex + 24).map((row, index) => (
+                <li key={index}>{row.fcstValue}°C</li>
+              ))}
+            </ul>
+            <ul>
+              <li style={{ width: "5rem", fontWeight: "700" }}>강수확률</li>
+              {rainProb.slice(startIndex, startIndex + 24).map((row, index) => (
+                <li key={index}>{row.fcstValue}%</li>
+              ))}
+            </ul>
+          </div>
+        </section>
+        <div>
+          <MainAirStatus cityName={weatherArea.cityName} />
         </div>
-        <div className='etc-box'>
-          <div>풍속 {windData[startIndex].fcstValue} m/s</div>
-          <div>습도 {humidityData[startIndex].fcstValue}%</div>
-        </div>
-        <div className='weather-slider overflow-auto'>
-          <ul>
-            <li style={{ width: '5rem', fontWeight: '700' }}>시간</li>
-            {tmpData.slice(startIndex, startIndex + 24).map((row, index) => (
-              <li key={index}>{row.fcstTime.substring(0, 2)}시</li>
-            ))}
-          </ul>
-          <ul>
-            <li style={{ width: '5rem', fontWeight: '700' }}>기온</li>
-            {tmpData.slice(startIndex, startIndex + 24).map((row, index) => (
-              <li key={index}>{row.fcstValue}°C</li>
-            ))}
-          </ul>
-          <ul>
-            <li style={{ width: '5rem', fontWeight: '700' }}>강수확률</li>
-            {rainProb.slice(startIndex, startIndex + 24).map((row, index) => (
-              <li key={index}>{row.fcstValue}%</li>
-            ))}
-          </ul>
-        </div>
-      </section>
+      </>
     );
   }
   return (
-    <div className='weather-section'>
+    <div className="weather-section">
       {content}
       <section></section>
     </div>
